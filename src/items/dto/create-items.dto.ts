64115c56c1b6ebf88c250a2698824c535130fc83
codebyalok <engg.alokg@gmail.com>
2020-06-04 10:26:42 +0100

    adding get items/id resource with item id in response and post with test response

---
+++ src/items/dto/create-items.dto.ts
@@ -0,0 +1,5 @@
+export class CreateItemsDTO{
+  readonly name: string;
+  readonly title: string;
+  readonly qty: number;
+}
--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateItemsDTO } from './dto/create-items.dto';
 
 @Controller('items')
 export class ItemsController {
@@ -6,6 +7,16 @@ export class ItemsController {
 @Get()
 findAll(): string {
   return "This is all items";
+}
+
+@Get(':id')
+findOne(@Param() param): string{
+  return `Item ${param.id}`
+}
+
+@Post()
+create(@Body () createItemDTO: CreateItemsDTO) : string {
+  return `Name: ${createItemDTO.name} Desc:${createItemDTO.title}`;
 }
 
 }
